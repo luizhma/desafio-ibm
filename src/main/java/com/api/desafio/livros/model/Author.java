e53cc4c0431fd8b88281dd61a9a7b0fd201824f7
suelitonoliveira <suelitondeoliveira@gmail.com>
2021-07-30 21:18:46 -0300

    fix: remove classe Livro

--- src/main/java/com/api/desafio/livros/model/Author.java
+++ src/main/java/com/api/desafio/livros/model/Author.java
@@ -6,16 +6,12 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import java.time.LocalDate;
-import java.util.List;
 
 
 @Data
@@ -41,7 +37,7 @@ public class Author {
     private String biography;
 
 
-    @OneToMany(cascade = CascadeType.PERSIST, mappedBy = "author")
-    private List<Livro> livros;
+ /*   @OneToMany(cascade = CascadeType.PERSIST, mappedBy = "author")
+    private List<Book> books;*/
 
 }
