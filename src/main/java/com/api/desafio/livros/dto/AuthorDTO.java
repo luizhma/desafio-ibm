7da62a1d5b4faf4215b043e0bcb3a4bd3780b101
marcelo-rebello1982 <58940851+marcelo-rebello1982@users.noreply.github.com>
2021-07-31 00:04:50 -0300

    fix: validaÃção DTO

--- src/main/java/com/api/desafio/livros/dto/AuthorDTO.java
+++ src/main/java/com/api/desafio/livros/dto/AuthorDTO.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
 import java.time.LocalDate;
 
 @Data
@@ -12,9 +13,10 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class AuthorDTO {
 
+    @NotEmpty(message = "Campo NOME é requerido")
     private String name;
+
     private String nationality;
     private String biography;
     private LocalDate birthDate;
-
 }
