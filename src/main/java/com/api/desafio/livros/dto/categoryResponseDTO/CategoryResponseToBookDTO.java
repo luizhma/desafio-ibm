10327e05db0739757faa281755d12e28197d4a61
luizhma <lhmalves@outlook.com>
2021-08-02 11:24:01 -0300

    chore: adicionado novo dto em category para ser usado no post de book

--- src/main/java/com/api/desafio/livros/dto/categoryResponseDTO/CategoryResponseToBookDTO.java
+++ src/main/java/com/api/desafio/livros/dto/categoryResponseDTO/CategoryResponseToBookDTO.java
@@ -1,2 +1,16 @@
-package com.api.desafio.livros.dto.categoryResponseDTO;public class CategoryResponseToBookDTO {
+package com.api.desafio.livros.dto.categoryResponseDTO;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryResponseToBookDTO implements Serializable {
+    @ApiModelProperty(value = "ID da Categoria")
+    private Long id;
 }
